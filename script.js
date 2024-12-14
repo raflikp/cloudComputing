d22e118264cc4a5ff59396db41a86bf9e301877e
raflikp <153498617+raflikp@users.noreply.github.com>
2024-12-14 16:52:00 +0700

    input files

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("actionButton").addEventListener("click", () => {
+    const message = document.getElementById("message");
+    message.textContent = "Hello! This website is live on Azure!";
+  });
+  
